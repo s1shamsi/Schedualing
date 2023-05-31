50a580a56e34dfe5663af1d5263ff1df2aeb3de0
s1shamsi <eng.saidj@gmail.com>
2023-05-31 16:19:04 +0400

    q45

--- src/main/java/com/Schedual/Scheduling/Schedual.java
+++ src/main/java/com/Schedual/Scheduling/Schedual.java
@@ -138,11 +138,11 @@ public class Schedual {
 //43-@Scheduled(cron = "0 30 16 ? 7,8 MON-FRI")
 
 
-//44-
-@Scheduled(cron = "0 0 0 1 1,4,7,10 ?")
+//44-@Scheduled(cron = "0 0 0 1 1,4,7,10 ?")
 
 
-//45- @Scheduled(cron = "")
+//45-
+@Scheduled(cron = "0 0 8 1-30 * ?")
 
 
 //46- @Scheduled(cron = "")
