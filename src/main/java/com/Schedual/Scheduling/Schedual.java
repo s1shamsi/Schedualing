427586ba58751718d6b209ac06bde585301f2d69
AliAlSubhi98 <alialsubhi1998@gmail.com>
2023-05-31 15:05:58 +0400

    task

---
+++ src/main/java/com/Schedual/Scheduling/Schedual.java
@@ -0,0 +1,163 @@
+package com.Schedual.Scheduling;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Schedual {
+
+//1-@Scheduled(cron = "")
+
+
+//2-@Scheduled(cron = "")
+
+
+//3-  @Scheduled(cron = "")
+
+
+//4-@Scheduled(cron = "")
+
+
+//5-@Scheduled(cron = "")
+
+
+//6-   @Scheduled(cron = "")
+
+
+//7- @Scheduled(cron = "")
+
+
+//8- @Scheduled(cron = "")
+
+
+//9-@Scheduled(cron = "")
+
+
+//10- @Scheduled(cron = "")
+
+
+//11-@Scheduled(cron = "")
+
+
+//12-@Scheduled(cron = "")
+
+
+//13- @Scheduled(cron = "")
+
+
+//14-@Scheduled(cron = "")
+
+
+//15-@Scheduled(cron = "")
+
+
+//16- @Scheduled(cron = "")
+
+
+//17- @Scheduled(cron = "")
+
+
+//18- @Scheduled(cron = "")
+
+
+//19-  @Scheduled(cron = "")
+
+
+//20-@Scheduled(cron = "")
+
+
+//21-  @Scheduled(cron = "")
+
+
+//22-@Scheduled(cron = "")
+
+
+//23-@Scheduled(cron = "")
+
+
+//24-@Scheduled(cron = "")
+
+
+//25- @Scheduled(cron = "")
+
+
+//26-  @Scheduled(cron = "")
+
+
+//27- @Scheduled(cron = "")
+
+
+//28-  @Scheduled(cron = "")
+
+
+//29-@Scheduled(cron = "")
+
+
+//30-@Scheduled(cron = "")
+
+
+//31- @Scheduled(cron = "")
+
+
+//32-    @Scheduled(cron = "")
+
+
+//33-    @Scheduled(cron = "")
+
+
+//34-@Scheduled(cron = "")
+
+
+//35-  @Scheduled(cron = "")
+
+
+//36- @Scheduled(cron = "")
+
+
+//37- @Scheduled(cron = "")
+
+
+//38- @Scheduled(cron = "")
+
+
+//39-@Scheduled(cron = "")
+
+
+//40-@Scheduled(cron = "")
+
+
+//41-    @Scheduled(cron = "")
+
+
+//42-  @Scheduled(cron = "")
+
+
+//43- @Scheduled(cron = "")
+
+
+//44-@Scheduled(cron = "")
+
+
+//45- @Scheduled(cron = "")
+
+
+//46- @Scheduled(cron = "")
+
+
+//47-@Scheduled(cron = "")
+
+
+//48-  @Scheduled(cron = "")
+
+
+//49-@Scheduled(cron = "")
+
+
+//50-@Scheduled(cron = "")
+
+    public void schedule1(){
+        System.out.println("Hello world, This Said from CodeLine");
+    }
+
+
+}
